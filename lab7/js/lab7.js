f0e308af97a83a7c9a3492298dc219b93a0b4d20
kimestr0304 <estradakimberly0303@gmail.com>
2024-10-25 23:51:37 -0700

    3rd changes

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -7,12 +7,15 @@
 function sortUserName() {
     var userName = window.prompt("Hello & welcome, may you please tell me your name and something exciting will occur.");
     console.log("userName =", userName);
+
     // split string to array
     var nameArray = userName.split('');
     console.log("nameArray =", nameArray);
+
     // sort the array
     var nameArraySort = nameArray.sort();
     console.log("nameArraySort =", nameArraySort);
+
     //join array back to a string
     var nameSorted = nameArraySort.join('');
     console.log("nameSorted =", nameSorted);
@@ -20,5 +23,5 @@ function sortUserName() {
 }
 
 // Output
-document.writeln("See something exciting did occur:",
-    sortUserName(), "</br>");
+document.getElementById("output").innerHTML = "See, something exciting did occur: " + result + "<br>";
+console.log("JavaScript file loaded successfully!");
